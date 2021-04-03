3626ed5f3ac1b7aac2761ab21b3ec6e6cf07bfd5
Tanecia Thompson <taneciathompson@gmail.com>
2021-04-03 12:28:23 -0400

    intial commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+// config/database.js
+module.exports = {
+  url: "mongodb+srv://mari27:code27@cluster0.3g7gm.mongodb.net/casinoCollection?retryWrites=true&w=majority",
+  dbName: "collect",
+};
+
+/*
+heroku config:set MONGODB_URI="mongodb+srv://wadeMongo:wade2021@cluster0.uusdg.mongodb.net/moodTracker?retryWrites=true&w=majority"
+*/
+
+//admin@admin.com
+//password
